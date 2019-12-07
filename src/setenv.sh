2d9a60851f357518af62d57e5b1636294c90d31d
Chrisyilin <58618424+Chrisyilin@users.noreply.github.com>
2019-12-07 14:38:03 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=Chrisyilin
 
 export MYCLUSTER=tektonknativebeijing12
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AC/.bluemix/plugins/container-service/clusters/tektonknativebeijing12/kube-config-tok02-tektonknativebeijing12.yml
 
 export NAMESPACE=chrisyilin
 export EMAIL=13661231863@163.com
-export INGRESS=
+export INGRESS=tektonknativebeijing12-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
